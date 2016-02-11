8c94731ac0f0604b7da5c5bbab43aaa1295dd2ab
Chris Deemer <cdeemer@crossview.com>
2016-02-11 00:17:06 -0800

    trying symlink from vagrant to /var/www

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,15 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant /var/www
 fi
 
-# # install NodeJS and NPM
-# wget -P ~/Downloads/ https://nodejs.org/dist/v4.3.0/node-v4.3.0-linux-x64.tar.xz
-# tar -xf ~/Downloads/node-v4.3.0-linux-x64.tar.xz -C /usr/local
-# #verify NodeJS and NPM
-# node -v
-# npm -v
-# # install Gulp globally
-# npm install --global gulp-cli
+# symlink shared dir onto www
+mv www www.orig
+ln -s /var/www /vagrant
+
+# install NodeJS and NPM
+wget -P ~/Downloads/ https://nodejs.org/dist/v4.3.0/node-v4.3.0-linux-x64.tar.xz
+tar -xf ~/Downloads/node-v4.3.0-linux-x64.tar.xz -C /usr/local
+#verify NodeJS and NPM
+node -v
+npm -v
+# install Gulp globally
+npm install --global gulp-cli
