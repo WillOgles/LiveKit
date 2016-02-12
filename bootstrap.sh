ff99fcbd53713cfaf82c02eda6ad82670db1e496
Chris Deemer <cdeemer@crossview.com>
2016-02-12 10:13:56 +0000

    patternlab installed. bashrc for terminal.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ if ! [ -L /var/www/html ]; then
   rm -rf /var/www/html
   ln -fs /vagrant /var/www/html
 fi
-
+apt-get install git-all
 # install NodeJS and NPM
 wget -nv -P ~/Downloads/ https://nodejs.org/dist/v4.3.0/node-v4.3.0-linux-x64.tar.xz
 tar --strip-components 1 -xf ~/Downloads/node-v4.3.0-linux-x64.tar.xz -C /usr/local
@@ -15,3 +15,5 @@ node -v
 npm -v
 # install Gulp globally
 npm install --global gulp-cli
+
+cp /vagrant/.bashrc /home/vagrant/.bashrc
